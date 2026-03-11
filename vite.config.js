4e3250efa04f73d7d0df71e1ce4dcff99f72c758
Mohamed Elsherif <melsherif137@gmail.com>
2026-03-11 16:22:35 +0200

    fix Vite base and image paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Elsherif-Store/",
   server: {
     port: 3000,
   },
