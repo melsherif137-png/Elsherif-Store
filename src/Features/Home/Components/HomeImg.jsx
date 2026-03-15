939f65210eefb57d2ddd526c0510fa8f598d617e
Mohamed Elsherif <melsherif137@gmail.com>
2026-03-15 05:30:04 +0200

    import img as file

--- src/Features/Home/Components/HomeImg.jsx
+++ src/Features/Home/Components/HomeImg.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import myImage from "../../../assets/Gemini_Generated_Image_yfzus8yfzus8yfzu-removebg-preview.png";
 const HomeImg = () => {
   return (
     <motion.div
@@ -7,10 +8,7 @@ const HomeImg = () => {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, ease: [0.25, 0.1, 0.25, 1], delay: 0.2 }}
     >
-      <img
-        src={`${process.env.PUBLIC_URL}/Gemini_Generated_Image_yfzus8yfzus8yfzu-removebg-preview.png`}
-        alt=""
-      />
+      <img src={myImage} alt="" />
     </motion.div>
   );
 };
