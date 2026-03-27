9a2c88c8459f2affe08eeafe11db2bd9f2de03f6
Mohamed Elsherif <melsherif137@gmail.com>
2026-03-27 02:53:19 +0200

    margin over

--- src/Features/Store/components/Category.jsx
+++ src/Features/Store/components/Category.jsx
@@ -84,7 +84,7 @@ const Category = ({
       <motion.div
         className="searchFilter"
         onClick={() => inputRef.current.focus()}
-        initial={{ opacity: 0, x: 40 }}
+        initial={{ opacity: 0, x: 20 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.1, ease: "easeOut", delay: 0.2 }}
       >
